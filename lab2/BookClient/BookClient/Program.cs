d698bafa07fe629c24529d84e14050ba7c14d6af
andreeaMathe <matheandreean@gmail.com>
2021-03-08 16:23:10 +0200

    Added .net core server and client

---
+++ lab2/BookClient/BookClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using BookService;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace BookClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(
+            //                  new HelloRequest { Name = "GreeterClient" });
+            //Console.WriteLine("Greeting: " + reply.Message);
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Bookstore.BookstoreClient(channel);
+
+            //var bookToBeAdded = new Book() { Title = "Poems" };
+            //var response = await client.AddBookAsync(
+            //                  new AddBookRequest { Book = bookToBeAdded });
+            //Console.WriteLine("Adding status: " + response.Status);
+
+            var response = await client.GetAllBooksAsync(new Empty());
+            Console.WriteLine(response);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2/BookService/BookService/Services/BookService.cs
@@ -0,0 +1,35 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.Services
+{
+    public class BookService : Bookstore.BookstoreBase
+    {
+        public override Task<AddBookResponse> AddBook(AddBookRequest request, ServerCallContext context)
+        {
+            var book = request.Book;
+            Console.WriteLine("Added book: " + book.Title);
+
+            return Task.FromResult(new AddBookResponse() { Status = AddBookResponse.Types.Status.Success });
+        }
+
+        public override Task<GetAllBooksResponse> GetAllBooks(Empty request, ServerCallContext context)
+        {
+            Console.WriteLine("GetAllBooks called");
+
+            var books = new List<Book>() {
+            new Book() { Title = "Homo Deus"},
+            new Book() { Title = "Sapiens"}
+            };
+
+            var response = new GetAllBooksResponse();
+            response.Books.AddRange(books);
+
+            return Task.FromResult(response);
+        }
+    }
+}
