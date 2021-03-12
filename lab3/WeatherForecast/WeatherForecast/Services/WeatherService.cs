e3208c03fc14a0867fc2d8098e64e663ac922486
andreeaMathe <matheandreean@gmail.com>
2021-03-12 09:05:05 +0200

    Added WeatherForecast example using streams.

---
+++ lab3/WeatherForecast/WeatherForecast/Services/WeatherService.cs
@@ -0,0 +1,39 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Services
+{
+    public class WeatherService : WeatherForecasts.WeatherForecastsBase
+    {
+        public override async Task GetWeatherStream(Empty request, IServerStreamWriter<WeatherData> responseStream, ServerCallContext context)
+        {
+            Random random = new Random();
+
+            try
+            {
+                while (!context.CancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(2000);
+
+                    var weatherData = new WeatherData()
+                    {
+                        DateTimeStamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                        DegreesCelsius = random.Next(-20, 35),
+                        Humidity = random.NextDouble(),
+                        Precipitation = random.NextDouble()
+                    };
+
+                    await responseStream.WriteAsync(weatherData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message + "\n");
+            }
+        }
+    }
+}
---
+++ lab3/WeatherForecastClient/WeatherForecastClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WeatherForecast;
+
+namespace WeatherForecastClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new WeatherForecasts.WeatherForecastsClient(channel);
+
+            var dataStream = client.GetWeatherStream(new Empty());
+            var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token;
+
+            try
+            {
+                await foreach (var weatherData in dataStream.ResponseStream.ReadAllAsync(cancellationToken))
+                {
+                    Console.WriteLine(weatherData.DateTimeStamp + ": " + weatherData.DegreesCelsius + "C, humidity "
+                        + weatherData.Humidity + "%");
+                }
+            }
+            catch (RpcException e) when (e.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled by client.");
+            }
+        }
+    }
+}
