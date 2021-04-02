35d6eccc23869f1508d6796e9ef701660afe6325
andreeaMathe <matheandreean@gmail.com>
2021-04-02 08:54:49 +0300

    Added basic WPF example with gRPC

---
+++ lab6/GreeterClient/GreeterClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using GreeterLibrary;
+using GreeterProtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GreeterClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloClicked(object sender, RoutedEventArgs e)
+        {
+            _ = CallGrpcService();
+        }
+
+        private async Task CallGrpcService()
+        {
+            var client = new GrpcServiceProvider().GetGreeterClient();
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "WPF client" });
+
+            this.HelloMessage.Text = reply.Message;
+        }
+    }
+}
---
+++ lab6/GreeterClient/GreeterLibrary/GrpcServiceProvider.cs
@@ -0,0 +1,21 @@
+﻿using GreeterProtos;
+using Grpc.Net.Client;
+using System;
+
+namespace GreeterLibrary
+{
+    public class GrpcServiceProvider
+    {
+        private string Url { get; set; }
+        private Lazy<GrpcChannel> RpcChannel { get; set; }
+        private Greeter.GreeterClient GreeterClient { get; set; }
+
+        public GrpcServiceProvider()
+        {
+            this.Url = "https://localhost:5001";
+            this.RpcChannel = new Lazy<GrpcChannel>(GrpcChannel.ForAddress(this.Url));
+        }
+
+        public Greeter.GreeterClient GetGreeterClient() => this.GreeterClient ??= new Greeter.GreeterClient(this.RpcChannel.Value);
+    }
+}
